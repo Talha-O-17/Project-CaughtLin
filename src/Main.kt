bbffd124bd49c403ca6d85f6debf30d6f7bc9ae4
Talha-O-17 <talhacse11bu13@gmail.com>
2025-08-11 02:04:04 +0600

    Introduction to var and Val

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,24 @@
-//This is entry point of the kotlin.
 fun main() {
-    //this the body of the function...
-    //Code in the body gets executed...
-    print("Hello World!!!")
-    //This prints Hello World to the terminal...
-    //Do the same code using println and observe the difference...
+    //variables in kotlin
+    // we will learn about var and val
+
+    //everything between double quotation is a string.
+    // so the variable, name , can store a string
+    var name = "Talha17"
+
+    //this should give print the name when executed..
+    println(name);
+
+    //Important thing about var is that value  can be changed
+    name = "Zeros-O-17"
+    println(name);
+
+    //we can explicitly specify the data type after it's name
+    val favColor : String = "Blue"
+    // since we have used val we can't change the value again like name
+
+    //if we didn't use $before the name the output would be something like this
+    // My name is name. And My favourite color is favColor
+    //So inside double quotation to the value of a variable we use $varName or ${varName}
+    println("My name is $name. And My favourite color is ${favColor}")
 }
